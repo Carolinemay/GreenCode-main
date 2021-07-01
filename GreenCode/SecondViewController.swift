aba9308a3f2e910683eabed56282d35c4e445b74
Caroline Zhou <czhou@students.chccs.k12.nc.us>
2021-07-01 11:03:31 -0400

    Initial Commit

---
+++ GreenCode/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  GreenCode
+//
+//  Created by Tarek Yassine on 6/30/21.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
---
+++ GreenCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GreenCode
+//
+//  Created by Shereen Yassine on 6/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBOutlet weak var greenCodeLogo: UIImageView!
+   
+    }
